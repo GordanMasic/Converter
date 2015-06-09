46aec09e12faab672498e23d69d1d2bd74ef6dc7
Mladen Teofilovic <mladen.teofilovic@BitCamps-Mac-mini-5.local>
2015-06-09 11:33:01 +0200

    Input metoda

--- ProjectConverter/src/MladenInput.java
+++ ProjectConverter/src/MladenInput.java
@@ -1,35 +1,30 @@
 import java.util.Scanner;
 
-
 public class MladenInput {
 
 	public static void inputCurrency() {
 		TextIO.readFile("src/Currency.txt");
 		System.out.println("Ponudjene valute za konverziju:");
 		while (!TextIO.eof()) {
-			String s= TextIO.getln();
+			String s = TextIO.getln();
 			String[] parts = s.split(" ");
 			String part1 = parts[0];
-			
-		System.out.println(part1);
+
+			System.out.println(part1);
 		}
 		TextIO.readStandardInput();
-		System.out.println("Koju valutu zelite pretvoriti .npr EUR");
-		Scanner in= new Scanner (System.in);
-		String value1= in.nextLine();
-		System.out.println("U koju valutu pretvarate: ");
-		String valute2=in.nextLine();		
-		System.out.println("Koliko"+value1+" zelite pretvoriti:");
-		int value= in.nextInt();
+		System.out.println("Which currency you want to convert: ");
+		Scanner in = new Scanner(System.in);
+		String currency1 = in.nextLine();
+		System.out.println("In which currency you want to convert: ");
+		String currency2 = in.nextLine();
+		System.out.println("How much " + currency1 + " you want to convert:");
+		double value = in.nextDouble();
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
 
 		inputCurrency();
-		
 
 	}
 
