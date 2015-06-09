7ed0ad857e90b1da44d2c393a406e0398c0e12c5
Mladen Teofilovic <mladen.teofilovic@BitCamps-Mac-mini-5.local>
2015-06-09 11:28:37 +0200

    Input metoda

--- ProjectConverter/src/MladenInput.java
+++ ProjectConverter/src/MladenInput.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 
 public class MladenInput {
 
-	public static void main(String[] args) {
-		
-
+	public static void inputCurrency() {
 		TextIO.readFile("src/Currency.txt");
 		System.out.println("Ponudjene valute za konverziju:");
 		while (!TextIO.eof()) {
@@ -19,6 +17,18 @@ public class MladenInput {
 		System.out.println("Koju valutu zelite pretvoriti .npr EUR");
 		Scanner in= new Scanner (System.in);
 		String value1= in.nextLine();
+		System.out.println("U koju valutu pretvarate: ");
+		String valute2=in.nextLine();		
+		System.out.println("Koliko"+value1+" zelite pretvoriti:");
+		int value= in.nextInt();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+		inputCurrency();
 		
 
 	}
